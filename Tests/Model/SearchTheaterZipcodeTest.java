e0dedeea429b7bcc358f99a814a46ee73ae0c2e8
Gerald Brady <117941447+gsb5249@users.noreply.github.com>
2023-04-22 15:49:30 -0400

    Test for theatre using zipcode test

--- Tests/Model/SearchTheaterZipcodeTest.java
+++ Tests/Model/SearchTheaterZipcodeTest.java
@@ -12,7 +12,7 @@ class SearchTheaterZipcodeTest {
 
     @Test
     void getZipCode__passString__returns_List_of_Theater_objectos() {
-        List<String> listOfNameParts = Arrays.asList("19847", "19020");
+        List<String> sampleZipcodes = Arrays.asList("19847", "19020");
         ArrayList<String> expected = new ArrayList<>();
         expected.add(Datastore.getTheaterById(1).toString());
         expected.add(Datastore.getTheaterById(2).toString());
