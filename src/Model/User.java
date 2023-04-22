0f218085c38550db8fd3ed3ebc798a3825f0595d
patelvivek9111 <129427387+patelvivek9111@users.noreply.github.com>
2023-04-22 09:59:42 -0400

    Update User.java

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Scanner;
+
 public class User extends DataStoreObj {
     private String firstName;
     private String lastName;
@@ -14,15 +16,35 @@ public class User extends DataStoreObj {
     }
 
     public void setFirstName(String firstName) {
+        Scanner input = new Scanner(System.in);
+        while (!firstName.matches("^[A-Z][a-z]*$")) {
+            System.out.println("Invalid input for first name. It must start with a capital letter and have only lowercase letters afterwards.");
+            System.out.print("Enter first name: ");
+            firstName = input.nextLine();
+        }
         this.firstName = firstName;
+        System.out.println("\n");
     }
 
     public void setLastName(String lastName) {
+        Scanner input = new Scanner(System.in);
+        while (!lastName.matches("^[A-Z][a-z]*$")) {
+            System.out.println("Invalid input for last name. It must start with a capital letter and have only lowercase letters afterwards.");
+            System.out.print("Enter last name: ");
+            lastName = input.nextLine();
+        }
         this.lastName = lastName;
+        System.out.println("\n");
     }
 
     public void setEmailAddress(String emailAddress) {
+        // Check if email address is valid using a regular expression
+        if (!emailAddress.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+            System.out.println("Invalid email address format.");
+            return;
+        }
         this.emailAddress = emailAddress;
+        System.out.println("\n");
     }
 
     @Override
@@ -30,3 +52,4 @@ public class User extends DataStoreObj {
         return String.format("%s %s (%s)", this.firstName, this.lastName, this.emailAddress);
     }
 }
+
